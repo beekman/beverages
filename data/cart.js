0aa7a2e474963ef2deef3c7d1d7863e0ea6eb7bd
Ben Beekman <bbeekman@gmail.com>
2019-10-08 16:54:46 -0700

    created order array literal

---
+++ data/cart.js
@@ -0,0 +1,28 @@
+const coffee = {
+    id: 'coffee',
+    quantity: 4,
+};
+
+const espresso = {
+    id: 'espresso',
+    quantity: 3
+};
+
+const cola = {
+    id: 'cola',
+    quantity: 5
+};
+
+const beer = {
+    id: 'beer',
+    quantity: 8
+};
+
+const beverages = [
+    coffee,
+    espresso,
+    cola,
+    beer
+];
+
+export default beverageOrder;
