715a748c38b439289b1d4946ff8633c7c3933338
Ben Beekman <bbeekman@gmail.com>
2019-10-08 17:00:45 -0700

    corrected order array

--- data/cart.js
+++ data/cart.js
@@ -1,28 +1,16 @@
-const coffee = {
+export const cart = [{
     id: 'coffee',
     quantity: 4,
-};
-
-const espresso = {
+},
+{
     id: 'espresso',
     quantity: 3
-};
-
-const cola = {
+},
+{
     id: 'cola',
     quantity: 5
-};
-
-const beer = {
+},
+{
     id: 'beer',
     quantity: 8
-};
-
-const beverages = [
-    coffee,
-    espresso,
-    cola,
-    beer
-];
-
-export default beverageOrder;
+}];
